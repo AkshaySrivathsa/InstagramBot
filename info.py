7fb97f000437bfbddb741d69642ea7ea0ee4bedc
Akshay Srivathsa <gamedev337@gmail.com>
2021-09-01 18:27:13 +0000

    likes function added

--- info.py
+++ info.py
@@ -1,3 +1,3 @@
-username = "your instagram username/email/number"
-password = "your instagram password"
+username = "your instagram account's username/email/number"
+password = "your instagram account's password"
 find_user = "username whose posts you want to like"
